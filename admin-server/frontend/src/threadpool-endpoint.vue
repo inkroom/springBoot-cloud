5c3254bbf9258b169175589c4a670e34bb61b9a3
inkbox <enpassPixiv@protonmail.com>
2021-08-06 16:12:21 +0800

    添加线程池监控，需要搭配客户端使用

---
+++ admin-server/frontend/src/threadpool-endpoint.vue
@@ -0,0 +1,62 @@
+<!--
+  - Copyright 2014-2018 the original author or authors.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+
+<template>
+  <div class="columns is-desktop">
+    <sba-panel :title="pool.name" v-for="pool in pools" :key="pool.name" class="column is-half-desktop">
+      <template v-slot:actions>
+        <router-link
+          :to="{ name: 'journal', query: { instanceId: instance.id } }"
+          class="button icon-button"
+        >
+          <font-awesome-icon icon="history" />
+        </router-link>
+      </template>
+      <table class="health-details table is-fullwidth">
+        <tr v-for="(value, key) in pool" :key="key">
+          <td v-if="key != 'name'">{{ key }}</td>
+          <td v-if="key != 'name'">{{ value }}</td>
+        </tr>
+      </table>
+    </sba-panel>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    instance: {
+      //<1>
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    pools: "",
+  }),
+  async created() {
+    const response = await this.instance.axios.get("actuator/threadPool"); //<2>
+    this.pools = response.data;
+  },
+};
+</script>
+
+<style>
+.custom {
+  font-size: 20px;
+  width: 80%;
+}
+</style>
